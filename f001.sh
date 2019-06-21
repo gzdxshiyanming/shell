b411473c19e5b2a9b79d5d07ad320d73cba215da
gzdxshiyanming <51870861+gzdxshiyanming@users.noreply.github.com>
2019-06-21 21:41:45 +0800

    Update f001.sh

--- f001.sh
+++ f001.sh
@@ -1,6 +1,17 @@
-jdk1.8.0_181
-export JAVA_HOME=/usr/local/jdk1.8.0_181
-export JRE_HOME=$JAVA_HOME/jre
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=./:JAVA_HOME/lib:$JRE_HOME/lib
-ifconfig
+#!/bin/bash
+echo '本机网卡信息为：'
+Elist=`ifconfig | grep ^[a-z]| awk -F":" '{print $1}'`
+for Eth in $Elist
+do
+    IP=`ifconfig $Eth | grep -w inet | awk '{print $2,$3,$4}'`
+    echo '    '$Eth:$IP
+done
+
+echo '本机的默认网关为：'
+route -n | grep ^0 | awk '{print "    "$2}'
+
+echo '本机的主机名为：'
+echo '    '`hostname`
+
+echo '本机的DNS为：'
+grep nameserver /etc/resolv.conf | awk '{print "    "$2}' 
